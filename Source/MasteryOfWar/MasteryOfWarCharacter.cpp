cc07b053c8659f3fbc12db2dc0af875b73ffcbea
Nikolai Telin <telinnikolai@gmail.com>
2024-10-30 23:31:18 +0000

    Some camera manipulations

--- Source/MasteryOfWar/MasteryOfWarCharacter.cpp
+++ Source/MasteryOfWar/MasteryOfWarCharacter.cpp
@@ -20,15 +20,21 @@ AMasteryOfWarCharacter::AMasteryOfWarCharacter()
 {
 	// Set size for collision capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
-		
+
 	// Don't rotate when the controller rotates. Let that just affect the camera.
-	bUseControllerRotationPitch = false;
-	bUseControllerRotationYaw = false;
-	bUseControllerRotationRoll = false;
+	// bUseControllerRotationPitch = false;
+	// bUseControllerRotationYaw = false;
+
+   bUseControllerRotationPitch = true;
+   bUseControllerRotationYaw = true;
+   bUseControllerRotationRoll = false;
 
 	// Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
+	//GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...
+
+  GetCharacterMovement()->bOrientRotationToMovement = false;
+
+  GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
 
 	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
 	// instead of recompiling to adjust them
@@ -40,18 +46,23 @@ AMasteryOfWarCharacter::AMasteryOfWarCharacter()
 	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
 
 	// Create a camera boom (pulls in towards the player if there is a collision)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
-	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+	// CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	// CameraBoom->SetupAttachment(RootComponent);
+	// CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
+	// CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
 	// Create a follow camera
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+	//FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
 
-	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
-	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
+  FollowCamera->SetupAttachment(GetCapsuleComponent());
+
+  //FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+
+  FollowCamera->bUsePawnControlRotation = true;
+  // Note: The skeletal mesh and anim blueprint references on the Mesh component
+  // (inherited from Character) are set in the derived blueprint asset named
+  // ThirdPersonCharacter (to avoid direct content references in C++)
 }
 
 void AMasteryOfWarCharacter::BeginPlay()
